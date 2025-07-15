e0e6a046f0ddfe30a941a786fb5bf22258f5404a
Ajay_00 <ajaylohith855@gmail.com>
2025-07-15 20:07:23 +0530

    Created Digital Clock using useEffect

---
+++ src/DigiClock.jsx
@@ -0,0 +1,44 @@
+import React,{useState,useEffect} from 'react'
+import './Styles/DigiClock.css'
+
+
+const DigiClock = () => {
+    const [time,setTime]=useState(new Date());
+
+    useEffect(()=>{
+        const interval=setInterval(()=>{
+            setTime(new Date());
+        },1000)
+        return ()=>{
+
+            clearInterval(interval)
+        }
+    },[]);
+
+    const formatTime=()=>{
+        let hours=time.getHours();
+        let minutes=time.getMinutes();
+        let seconds=time.getSeconds();
+        let merid=hours>=12?"PM":"AM";
+
+        hours=hours%12||12
+        return `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}${merid}`
+    }
+
+    const addZero=(num)=>{
+        return (num<10?"0":"")+num;
+    }
+
+
+  return (
+    <div className="clock-container">
+        <div className="clock">
+            <span>{formatTime()}</span>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default DigiClock
